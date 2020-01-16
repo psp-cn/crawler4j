7b05e4706faf0b8063759742677b3d74fd916642
codefix-service-user <vadiraja.k+codefix@codenation.co.in>
2020-01-16 14:18:11 +0000

    CodeFix#268-66936921-1635613-794

--- crawler4j-examples/crawler4j-examples-base/src/test/java/edu/uci/ics/crawler4j/examples/basic/BasicCrawler.java
+++ crawler4j-examples/crawler4j-examples-base/src/test/java/edu/uci/ics/crawler4j/examples/basic/BasicCrawler.java
@@ -1,5 +1,6 @@
 package edu.uci.ics.crawler4j.examples.basic;
 
+import edu.uci.ics.crawler4j.examples.AutoFixClass;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Pattern;
@@ -66,17 +67,8 @@ public class BasicCrawler extends WebCrawler {
         logger.debug("Path: '{}'", path);
         logger.debug("Parent page: {}", parentUrl);
         logger.debug("Anchor text: {}", anchor);
-
-        if (page.getParseData() instanceof HtmlParseData) {
-            HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
-            String text = htmlParseData.getText();
-            String html = htmlParseData.getHtml();
-            Set<WebURL> links = htmlParseData.getOutgoingUrls();
-
-            logger.debug("Text length: {}", text.length());
-            logger.debug("Html length: {}", html.length());
-            logger.debug("Number of outgoing links: {}", links.size());
-        }
+        AutoFixClass autoFix0 = new AutoFixClass(page);
+        autoFix0.autoFixMethod0();
 
         Header[] responseHeaders = page.getFetchResponseHeaders();
         if (responseHeaders != null) {
