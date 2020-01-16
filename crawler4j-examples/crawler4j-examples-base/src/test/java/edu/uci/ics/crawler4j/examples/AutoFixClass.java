e4b303f6c9679ec321dfd3b452df59c2e8b533da
codefix-service-user <vadiraja.k+codefix@codenation.co.in>
2020-01-16 14:13:30 +0000

    CodeFix#268-66936920-1635613-793

---
+++ crawler4j-examples/crawler4j-examples-base/src/test/java/edu/uci/ics/crawler4j/examples/AutoFixClass.java
@@ -0,0 +1,35 @@
+package edu.uci.ics.crawler4j.examples;
+
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+
+public class AutoFixClass {
+    CrawlController controller;
+    CrawlConfig config;
+
+    public AutoFixClass(CrawlConfig config) {
+        this.config = config;
+    }
+
+    public CrawlController getController() {
+        return controller;
+    }
+
+    public void setController(CrawlController controller) {
+        this.controller = controller;
+    }
+
+    public void autoFixMethod0() throws Exception {
+        PageFetcher pageFetcher = new PageFetcher(config);
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+        CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+        // For each crawl, you need to add some seed urls. These are the first
+        // URLs that are fetched and then the crawler starts following links
+        // which are found in these pages
+        controller.addSeed("https://www.ics.uci.edu/");
+    }
+}
