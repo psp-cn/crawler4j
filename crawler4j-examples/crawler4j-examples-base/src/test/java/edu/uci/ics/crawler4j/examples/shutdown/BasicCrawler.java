b4a42ded0cd535e53f07327d84f4363be5012b07
codefix-service-user <vadiraja.k+codefix@codenation.co.in>
2020-01-22 06:39:50 +0000

    CodeFix#[CC-877106]-268-68017111-1697354-833

--- crawler4j-examples/crawler4j-examples-base/src/test/java/edu/uci/ics/crawler4j/examples/shutdown/BasicCrawler.java
+++ crawler4j-examples/crawler4j-examples-base/src/test/java/edu/uci/ics/crawler4j/examples/shutdown/BasicCrawler.java
@@ -58,11 +58,11 @@ public class BasicCrawler extends WebCrawler {
         logger.debug("Docid of parent page: {}", parentDocid);
 
         if (page.getParseData() instanceof HtmlParseData) {
-            HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
-            String text = htmlParseData.getText();
-            String html = htmlParseData.getHtml();
-            Set<WebURL> links = htmlParseData.getOutgoingUrls();
-
+            AutoFixClass autoFix0 = new AutoFixClass(page);
+            autoFix0.autoFixMethod0();
+            text = autoFix0.getText();
+            links = autoFix0.getLinks();
+            html = autoFix0.getHtml();
             logger.debug("Text length: {}", text.length());
             logger.debug("Html length: {}", html.length());
             logger.debug("Number of outgoing links: {}", links.size());
